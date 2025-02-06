cb2d0dfc266012565350c0c7324b91c7479ce436
Muhammad Haris <haris72.info@gmail.com>
2025-02-06 16:36:34 +0500

    model of order , Reviews, Payment

--- e_BookStore/admin.py
+++ e_BookStore/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Profile
-from .models import Book
-from .models import Category,Cart
+from .models import *
+
 
 admin.site.register(Category)
 admin.site.register(Book)
 admin.site.register(Profile)
 admin.site.register(Cart)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(Payment)
--- e_BookStore/models.py
+++ e_BookStore/models.py
@@ -43,3 +43,47 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.book.title} ({self.quantity})"
+
+from django.contrib.auth.models import User
+from django.db import models
+from .models import Book
+#  order model get by the user
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    books = models.ManyToManyField(Book)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user.username} - {self.status}"
+
+# Review model given by the user for a books and display other
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(choices=[(i, i) for i in range(1, 6)])  # 1 to 5 rating
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.book.title}"
+
+
+class Payment(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=100)
+    payment_status = models.CharField(max_length=100, choices=[('pending', 'Pending'), ('completed', 'Completed'), ('failed', 'Failed')], default='pending')
+    payment_date = models.DateTimeField(auto_now_add=True)
+    transaction_id = models.CharField(max_length=255, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"Payment for Order {self.order.id} - {self.payment_status}"
